22756294c3d5a75cbe242b42fa1e3a03d62912f6
engallote <53823323+engallote@users.noreply.github.com>
2020-01-14 21:18:11 +0900

    Update 16112.java

--- 16112.java
+++ 16112.java
@@ -7,7 +7,6 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
